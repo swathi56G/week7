976479a45a8c4218c78b0e1d408b19e3c53c86dd
swathi56G <swathigopireddy56@gmail.com>
2026-09-08 20:27:32 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('registration.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    student_id = request.form['student_id']
+    email = request.form['email']
+    year = request.form['year']
+
+    return render_template('Successful.html', name=name, student_id=student_id, email=email, year=year)
+
+if __name__ == '__main__':
+    app.run(debug=True)
